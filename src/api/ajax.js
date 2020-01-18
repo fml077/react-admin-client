688ab9398c0225324f0046c589f572d29d3ff10a
fml <fangmeiliang@cmcm.com>
2020-01-18 19:01:16 +0800

    优化ajax请求接口封装函数

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -14,20 +14,36 @@ url:请求URL地址
 data:请求参数（默认为空）
 type:请求类型（默认GET）
 */
-export default function ajax(url, data={}, type='GET' ) {
+export default function ajax(url, params={}, type='GET' ) {
   return new Promise((resolve, reject) => {
     let promise
+    const headers = {
+      Accept: 'application/json', // 通过头指定，获取的数据类型是JSON
+    };
+    if (localStorage.getItem('access_token')) {
+      // 传输token
+      headers.Authorization = localStorage.getItem('access_token');
+    }
     // 1. 执行异步ajax请求
     if (type === 'GET') { // 发送GET请求
-      promise = axios.get(url, {
-        params: data
+      promise = axios({
+        method: 'get',
+        url: url,
+        params: params,
+        headers: headers,
       })
     } else {
-      promise = axios.post(url, data) // 发送POST请求
+      // promise = axios.post(url, data) // 发送POST请求
+      promise = axios({
+        method: 'post',
+        url: url,
+        params: params,
+        headers: headers,
+      })
     }
     // 2. 成功，调用resolve(value)
     promise.then(response => {
-      resolve(response)
+      resolve(response.data)
     }).catch(error => {
     // 3. 失败，不调用reject(error),而是提示异常信息
       message.error('请求出错了：', error.message)
